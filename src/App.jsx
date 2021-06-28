f95d12a0bfcc08e2f864b170fe7bd0895d7484d5
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-28 14:07:32 +0200

    React Three JS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import styled from "styled-components";
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%
+`;
+
+function App() {
+  return <CanvasContainer>Hello from Three.js</CanvasContainer>;
+}
+
+export default App;
